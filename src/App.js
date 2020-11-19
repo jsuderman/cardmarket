75455e221d473c0ee86179e54624f932a9b33a7f
Justin Suderman <jsuderman1@gmail.com>
2020-11-18 16:32:37 -0800

    added banner to header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./components/FontawesomeIcons/index"
 
 import Navigation from "./components/Navbar/Navigation";
+import Home from "./components/Home/Home";
 
 // import Landing from "./pages/Landing";
 
@@ -13,16 +14,22 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+
           <Route path="/checkout">
+            <Navigation />
             <h1>checkout</h1>
           </Route>
+
           <Route path="/login">
             <h1>login</h1>
           </Route>
+
           <Route path="/">
             <Navigation />
+            <Home />
             <h1>home page</h1>
           </Route>
+
         </Switch>
     </div>
     </Router>
